1985ec2025b11147ce0fd662af564d920e437870
nischal2052 <78245209+nischal2052@users.noreply.github.com>
2021-11-20 20:55:51 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_z8ON05cBC'', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-west-2_z8ON05cBC', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3hd80i81ojf5vcu43enp8n7bmo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
